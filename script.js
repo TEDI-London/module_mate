5eafe074c1892423a9e8ec3a0f0c6500665e3894
Vivian Bakiris <vivian.bakiris@hotmail.com>
2021-01-24 12:37:54 +1100

    Added functionality to save button to capture all current edges. Removed static hard coded module data.

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 $("document").ready(function(){
 
-var mods = ["Yo","Yoo","Yooo"];
-
-mods.forEach((item) => {
-  console.log(item)
-})
 
   //Pull List of Modules
   $.ajax({url: "modules.json", success: function(result){
@@ -13,56 +8,6 @@ mods.forEach((item) => {
      console.log(item.Module_Title)
    })
  }});
-
-  // create an array with nodes
-  var ENG1101 = new vis.DataSet([
-  { id: 1, label: "PS10100" },
-  { id: 2, label: "PS10101" },
-  { id: 3, label: "DI10100" },
-  { id: 4, label: "PS10102" },
-  { id: 5, label: "DI10101" },
-  { id: 6, label: "PS10103" },
-  { id: 7, label: "PS10104" },
-  { id: 8, label: "DI10102" },
-  { id: 9, label: "DI10103" },
-  { id: 10, label: "DI10104" },
-  { id: 11, label: "DI10105" },
-  { id: 12, label: "DI10106" },
-  { id: 13, label: "DI10108" },
-  { id: 14, label: "ST10100" },
-  { id: 15, label: "ST10101" },
-  { id: 16, label: "ST10102" }
-  ]);
-
-  var ENG1102 = new vis.DataSet([
-  { id: 20, label: "ST10103" },
-  { id: 21, label: "ST10104" },
-  { id: 22, label: "ST10105" },
-  { id: 23, label: "ST10106" },
-  { id: 24, label: "EE10101" },
-  { id: 25, label: "EE10102" },
-  { id: 26, label: "EE10103" },
-  { id: 27, label: "EE10104" },
-  { id: 28, label: "EE10105" },
-  { id: 29, label: "ME10103" },
-  { id: 30, label: "ME10104" },
-  { id: 31, label: "ME10105" },
-  { id: 32, label: "ME10106" },
-  { id: 33, label: "BS10100" }
-  ]);
-
-  var ENG1201 = new vis.DataSet([
-    { id: 34, label: "DI10107" },
-    { id: 35, label: "ST10107" },
-    { id: 36, label: "EE10106" },
-    { id: 37, label: "EE10107" },
-    { id: 38, label: "SU10100" },
-    { id: 39, label: "CS10100" },
-    { id: 40, label: "CS10101" },
-    { id: 41, label: "CS10102" },
-    { id: 42, label: "CS10103" }
-
-  ])
 
 
 
@@ -149,6 +94,9 @@ mods.forEach((item) => {
 
   var nodeCounter = 0;
 
+  var liveNodes;
+  var displayedNodes;
+  var network;
 
   $("#modules").on('change',function(){
     console.log(this.value);
@@ -158,36 +106,52 @@ mods.forEach((item) => {
     $.ajax({url: "nodes.json", success: function(result){
       console.log(result)
 
-      const liveNodes = result.filter(node => node.Module == current)
-
-      console.log(liveNodes)
+      liveNodes = result.filter(node => node.Module == current)
       //for each of these nodes we need to append it to the dataset object, or rather create a data set object.
-
       //Create a new dataset
 
-      let currentnodes = new vis.DataSet();
-      console.log(typeof(liveNodes))
+      var currentnodes = new vis.DataSet();
 
       //Then for each of the life nodes, current nodes.add
 
       liveNodes.forEach(function(node){
         nodeCounter = nodeCounter + 1;
-        currentnodes.add([{id:nodeCounter, label:node.Node_Title, shape:"dot"}])
+        currentnodes.add([{id:node.Code, label:node.Node_Title, shape:"dot", color:"#f0d700"}])
       })
+
+      //perhaps write the code to pull the edges too.
 
       var data = {
         nodes: currentnodes,
         edges: edges,
       };
 
-        var network = new vis.Network(container, data, options);
+      network = new vis.Network(container, data, options);
 
-
-
-
+      displayedNodes = currentnodes;
     }});
 
+
+
   })
+
+    $("#save").on('click', function(){
+      var exportEdges =[{}]
+      console.log("clicked!")
+      console.log(liveNodes);
+      liveNodes.forEach(node=>{
+
+        console.log(node.Code)
+        console.log(network.getConnectedNodes(node.Code,"to"));
+        exportEdges[node.Code] = network.getConnectedNodes(node.Code,"to");
+
+      })
+
+      console.log("The export edges are");
+      console.log(exportEdges)
+
+      //write to a file
+    })
 
 
 
